bbf5783bc14004e9e097b800b1a96b04dd1b7f61
cyn <cyndb@debian-laptop.miguelin>
2014-09-16 15:59:23 -0300

    Fix, the tag searched was a span, not a div

--- rq.py
+++ rq.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 
 
 filename = sys.argv[1]
-print filename
 
 programs_file = open(filename)
 programs = programs_file.readlines()
@@ -19,17 +18,19 @@ more_than_one_program = []
 for program in programs:
     result = requests.get("http://www.shouldiremoveit.com/programs.aspx?q=%s" % program)
     soup = BeautifulSoup(result.content)
-    found = soup.find_all(name="div", attrs="programbartxt_keep")
-    if found:
-        if len(found) > 1:
+    program_found = soup.find_all(name="span", class_="programbartxt_remove")
+    print program_found
+
+    if program_found:
+        if len(program_found) > 1:
             more_than_one_program.append(program)
         else:
-            percentage = found.text.split("% remove")[0]
-            if float(percentage) > 80:
-                should_remove.append(program)
+            for found in program_found:
+                percentage = found.text.split("% remove")
+                if float(percentage) > 80:
+                    should_remove.append(program)
     else:
-            not_found.append(program)
-
+        not_found.append(program)
 
 print "you should remove: %s" % str(should_remove).strip("[]")
 print "These programs were not found: %s" % str(not_found).strip("[]")
